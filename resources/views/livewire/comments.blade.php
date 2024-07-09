5baf300ea008fad3a93a07ecb3a24341cefbd002
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-07-09 11:08:57 +0300

    Fix comments width on safari browser

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,6 +1,14 @@
-<div>
-    <div id="fb-root"></div>
-    <div class="fb-comments commentsdark" data-colorscheme="dark" data-href="{{ request()->url() }}" data-width="100%" data-numposts="5"></div>
+<div class="w-full block relative">
+    <div id="fb-root w-full block relative"></div>
+    <div class="fb-comments" data-colorscheme="dark" data-href="{{ request()->url() }}" data-width="100%" data-numposts="5"></div>
+
+    {{-- a Fix for width on safari browser --}}
+    <style>
+        .fb_iframe_widget_fluid_desktop, .fb_iframe_widget_fluid_desktop span, .fb_iframe_widget_fluid_desktop iframe {
+            max-width: 100% !important;
+            width: 100% !important;
+        }
+    </style>
     
     @assets
         <script src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v5.0"></script>
