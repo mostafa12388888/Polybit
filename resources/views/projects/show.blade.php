5fbd1ec9a0b5d1348ab79a530a6eca28bfd3bc03
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-08-24 19:16:28 +0300

    Update project slider image's size

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -27,7 +27,7 @@
                             <ul class="glide__slides w-full">
                                 @foreach ($project->images as $image)
                                     <li class="glide__slide h-full relative">
-                                        <x-curator-glider :media="$image" format="webp" width="1920" height="1080" fit="contain" quality="70" class="w-full aspect-video object-contain bg-white" :alt="$project->title" />
+                                        <x-curator-glider :media="$image" format="webp" width="1920" height="1080" fit="crop" quality="70" class="w-full aspect-video object-cover" :alt="$project->title" />
                                     </li>
                                 @endforeach
                             </ul>
