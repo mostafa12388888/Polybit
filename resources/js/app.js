c602ac449a5a4cfced6b56039f692d800adaeb32
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-08-24 18:56:40 +0300

    Remove product images' zoom

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,6 @@
 import './bootstrap';
 import Cookies from 'js-cookie';
 import Glide from '@glidejs/glide';
-import ImageZoom from 'js-image-zoom';
 
 window.Cookies = Cookies;
 window.Glide = Glide;
-window.ImageZoom = ImageZoom;
--- resources/views/products/partials/_product-images.blade.php
+++ resources/views/products/partials/_product-images.blade.php
@@ -9,25 +9,9 @@
                 'alt': '{{ $image->alt ?? $product->name }}',
             },
         @endforeach
-    ],
-    initImageZoom () {
-        if(typeof imageZoomObj != 'undefined') {
-            imageZoomObj.kill()
-        }
-        
-        setTimeout(() => {
-            imageZoomObj = new ImageZoom(this.$el, {
-                fillContainer: true,
-                scale: 1.3,
-                zoomStyle: 'border-radius: .375rem',
-                zoomLensStyle: 'border-radius: .375rem; background: black; opacity: .1',
-                offset: {vertical: 0, horizontal: 10},
-                zoomPosition: '{{ direction() == 'ltr' ? 'right' : 'left' }}'
-            })
-        }, 100);
-    }
+    ]
 }" x-init="activeImage = images[0]">
-    <div x-init="$watch('activeImage', () => initImageZoom()); $nextTick(() => initImageZoom())">
+    <div>
         <div class="overflow-hidden">
             <x-img class="w-full sm:rounded-md min-h-72" x-bind:src="activeImage.full" x-bind:alt="activeImage.alt" />
         </div>
