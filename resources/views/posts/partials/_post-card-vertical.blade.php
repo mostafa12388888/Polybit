a8e7b28c7bbd7b9c3da632526746347bd3b43b42
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-08-24 16:54:59 +0300

    Update images background

--- resources/views/posts/partials/_post-card-vertical.blade.php
+++ resources/views/posts/partials/_post-card-vertical.blade.php
@@ -1,6 +1,6 @@
 <x-link class="group flex-grow flex flex-col text-start !p-0 bg-white dark:bg-dark-700/70 dark:shadow-none overflow-hidden !rounded-md" :href="route('posts.show', $post)">
     <div class="relative">
-        <x-curator-glider fallback="logo" :media="$post->image" format="webp" width="480" height="280" fit="fill" bg="FFFFFF" quality="70" class="w-full max-sm:aspect-video sm:h-44 md:h-56 lg:h-72 xl:h-56 2xl:h-72 object-cover" :alt="$post->title" />
+        <x-curator-glider fallback="logo" :media="$post->image" format="webp" width="480" height="280" fit="fill" bg="white" quality="70" class="w-full max-sm:aspect-video sm:h-44 md:h-56 lg:h-72 xl:h-56 2xl:h-72 object-cover" :alt="$post->title" />
 
         <div class="group-hover:opacity-100 opacity-0 transition-opacity absolute w-full h-full top-0 left-0 bg-dark-900/60 flex items-center justify-center text-white">
             <x-icons.link class="!w-9 !h-9" stroke-width="1.5" />
