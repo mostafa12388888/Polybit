1ea3143fdebf648c117be7834485018d41ffab1e
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-08-24 17:22:04 +0300

    Update images fit

--- resources/views/projects/partials/_project.blade.php
+++ resources/views/projects/partials/_project.blade.php
@@ -1,6 +1,6 @@
 <x-link href="{{ route('projects.show', $project) }}" class="group flex flex-col text-start !p-0 bg-white dark:bg-dark-700/70 dark:shadow-none shadow !rounded-md overflow-hidden w-full">
     <div class="relative">
-        <x-curator-glider fallback="logo" :media="$project->image" format="webp" width="480" height="280" fit="fill" bg="white" quality="70" class="w-full aspect-video object-cover" :alt="$project->title" />
+        <x-curator-glider fallback="logo" :media="$project->image" format="webp" width="480" height="280" fit="contain" bg="white" quality="70" class="w-full aspect-video object-cover" :alt="$project->title" />
 
         <div class="group-hover:opacity-100 opacity-0 transition-opacity absolute w-full h-full top-0 left-0 bg-dark-900/60  flex items-center justify-center text-white">
             <x-icons.link class="!w-9 !h-9" stroke-width="1.5" />
