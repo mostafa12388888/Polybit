40fd13760e55068933ebb68400039f9af4a145a0
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-09-08 22:21:28 +0300

    Fix typo in page view

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -17,7 +17,7 @@
         <div class="container mx-auto flex flex-col lg:flex-row sm:gap-4 max-sm:divide-y">
             <div class="lg:rounded-md flex-grow w-full lg:w-8/12 2xl:w-9/12 overflow-hidden">
                 <div class="prose prose-zinc dark:prose-invert bg-white dark:bg-dark-700/60 py-8 px-4 md:px-6 xl:px-8 max-w-full">
-                    {{ html($page->body) }}
+                    {!! html($page->body) !!}
                 </div>
             </div>
         </div>
