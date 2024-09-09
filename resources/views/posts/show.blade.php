d2967f6c3cb83372e3b2a0a29eec458fdea6defc
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-09-09 16:39:36 +0300

    Increase post's featured image load priority

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,10 +7,14 @@
 
     <x-slot name="image">{!! $post->meta('image') !!}</x-slot>
 
+    <x-slot name="head">
+        <link rel="preload" as="image" href="{!! $post->meta('image') !!}" fetchpriority="high" />
+    </x-slot>
+    
     @if (! $post->translated())
         <link rel="canonical" href="{{ localized_url($post->locales()[0] ?? app()->getLocale()) }}" />
     @endif
-
+    
     <x-slot name="heading">{{ $post->title }}</x-slot>
 
     <x-slot name="subheading">
@@ -38,7 +42,7 @@
     <article class="flex-grow bg-primary-100 dark:bg-dark-800/70 sm:p-2 md:p-4 lg:py-6 xl:py-8 relative">
         <div class="container mx-auto flex flex-col lg:flex-row sm:gap-4 max-sm:divide-y">
             <div class="lg:rounded-md flex-grow w-full lg:w-8/12 2xl:w-9/12 overflow-hidden">
-                <img src="{{ $post->meta('image') }}" class="w-full" alt="{{ $post->meta('image-alt') }}">
+                <img src="{{ $post->meta('image') }}" class="w-full" alt="{{ $post->meta('image-alt') }}" fetchpriority="high">
 
                 <div class="prose prose-zinc dark:prose-invert bg-white dark:bg-dark-700/60 py-8 px-4 md:px-6 xl:px-8 max-w-full">
                     {!! html($post->body) !!}
