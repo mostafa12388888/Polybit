785ec1dc56db877159c421bddcb0352bc17a6f76
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-09-09 20:29:42 +0300

    Update product specs design

--- resources/views/products/partials/_specs.blade.php
+++ resources/views/products/partials/_specs.blade.php
@@ -1,28 +1,42 @@
-<div class="lg:p-6 lg:bg-white lg:dark:bg-dark-700/40 flex flex-col gap-10 rounded-b-md lg:border lg:border-t-0 border-dark-200 dark:border-dark-600/50">
-    @foreach ($specs as $spec)
-        <div class="max-lg:!flex flex flex-col gap-4" x-show="tab == {{ $loop->index + 1 }}">
-            <h2 class="lg:hidden font-semibold text-xl lg:text-2xl text-dark-800 dark:text-dark-100 leading-tight">{{ $spec->title }}</h2>
-
-            <div class="prose prose-zinc dark:prose-invert max-w-full">
-                {!! html($spec->description) !!}
+<div class="lg:p-6 lg:bg-white lg:dark:bg-dark-700/40 flex flex-col lg:gap-10 rounded-t-md rounded-b-md border lg:border-t-0 border-dark-100 dark:border-dark-600/50">
+    @foreach ($specs as $spec)    
+        <x-button styling="light-link" class="lg:hidden !shadow-none rounded-none flex-grow py-5 bg-gray-100 dark:bg-dark-700/40 dark:focus:!brightness-100 w-full border-white dark:border-dark-700 flex items-center gap-2"
+            x-bind:class="{'border-b': tab != {{ $loop->index + 1 }} && {{ $loop->last ? 'false' : 'true' }}}"
+            @click="tab = {{ $loop->index + 1 }}">
+            <div class="text-gray-400 transition-all"
+                x-bind:class="{'{{ direction() == 'rtl' ? 'rotate-90' : '-rotate-90' }}': tab != {{ $loop->index + 1 }}}"
+            >
+                <x-icons.chevron-down x-cloak class="!w-5 !h-5" stroke-width="1.2" />
             </div>
-
-            <div class="flex flex-col gap-4">
-                @foreach ($spec->media->where('pivot.type', app()->getLocale()) as $file)
-                    <div class="flex gap-4 flex-wrap items-center justify-between border lg:border-none rounded-md p-4 border-dark-100 dark:border-dark-700/50">
-                        <p>{{ $file->title }}</p>
-
-                        <span class="flex-grow border-t border-dashed border-dark-200 dark:border-dark-700 max-lg:hidden"></span>
-
-                        <div class="flex w-full sm:w-auto gap-2 flex-wrap">
-                            <x-link href="{{ $file->getSignedUrl() }}" target="_blank" download="{{ $file->title }}" styling="light" class="flex items-center justify-center gap-2 flex-grow dark:lg:!bg-dark-600">
-                                <x-icons.download class="!w-5 !h-5" />
-                                <span>{{ __('Download') }}</span>
-                                <span class="uppercase">[{{ $file->ext }}]</span>
-                            </x-link>
-                        </div>
+            
+            <span>{{ $spec->title }}</span>
+        </x-button>
+        
+        <div class="lg:!duration-0 lg:!transition-none" x-show="tab == {{ $loop->index + 1 }}" x-collapse>
+            <div class="flex flex-col gap-4 max-lg:px-4 max-lg:py-6">
+                @if ($spec->description)
+                    <div class="prose prose-zinc dark:prose-invert max-w-full px-4">
+                        {!! html($spec->description) !!}
                     </div>
-                @endforeach
+                @endif
+    
+                <div class="flex flex-col gap-4">
+                    @foreach ($spec->media->where('pivot.type', app()->getLocale()) as $file)
+                        <div class="flex gap-4 flex-wrap items-center justify-between rounded-md lg:p-4">
+                            <p>{{ $file->title }}</p>
+    
+                            <span class="flex-grow border-t border-dashed border-dark-200 dark:border-dark-700 max-lg:hidden"></span>
+    
+                            <div class="flex w-auto gap-2 flex-wrap">
+                                <x-link href="{{ $file->getSignedUrl() }}" target="_blank" download="{{ $file->title }}" styling="light" class="flex items-center justify-center gap-2 flex-grow dark:lg:!bg-dark-600">
+                                    <x-icons.download class="!w-5 !h-5" />
+                                    <span class="max-lg:hidden">{{ __('Download') }}</span>
+                                    <span class="uppercase">[{{ $file->ext }}]</span>
+                                </x-link>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     @endforeach
