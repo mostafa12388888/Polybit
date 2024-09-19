b3326f8be1ecc6f510ab8160774510e9cd7acbdf
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2024-09-19 10:56:39 +0300

    Fix typo

--- app/Http/Middleware/RedirectPublicPath.php
+++ app/Http/Middleware/RedirectPublicPath.php
@@ -21,7 +21,7 @@ class RedirectPublicPath
         $new_url = preg_replace('/^\/index.php/', '', $new_url);
 
         if ($url != $new_url) {
-            return redirect()->to(config('app.url').'$new_url');
+            return redirect()->to(config('app.url').$new_url);
         }
 
         return $next($request);
