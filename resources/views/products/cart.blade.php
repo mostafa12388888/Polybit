d2e849851bda3e3f88b82a501e92a22e353f5c68
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2025-01-20 18:22:13 +0200

    Add gtag script to cart page

--- resources/views/products/cart.blade.php
+++ resources/views/products/cart.blade.php
@@ -79,3 +79,23 @@
         </div>
     </div>
 </section>
+
+<x-slot name="scripts">
+    <!-- Event snippet for عملية شراء conversion page
+    In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+    <script>
+        function gtag_report_conversion(url) {
+            var callback = function () {
+                if (typeof(url) != 'undefined') {
+                    window.location = url;
+                }
+            };
+            gtag('event', 'conversion', {
+                'send_to': 'AW-16825848814/-GO9CNTouowaEO6nmNc-',
+                'transaction_id': '',
+                'event_callback': callback
+            });
+            return false;
+        }
+    </script>
+</x-slot>
