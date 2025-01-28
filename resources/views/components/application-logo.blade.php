11f25d3c0bb5f47756b823c176fe8c3602447345
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2025-01-28 13:24:49 +0200

    Update logo component to allow uploading svg logos

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -8,10 +8,15 @@
         $darkmode_logo_url = $darkmode_logo->getSignedUrl(['w' => $width, 'fm' => 'webp'], false);
         $original_height = $darkmode ? $darkmode_logo->height : $logo->height;
         $original_width = $darkmode ? $darkmode_logo->width : $logo->width;
-        $height = ceil($width * $original_height / $original_width);
+        $height = $original_width ? ceil($width * $original_height / $original_width) : null;
     @endphp
 
-    <x-img src="{!! $darkmode ? $darkmode_logo_url : $logo_url !!}" x-bind:src="darkMode ? '{!! $darkmode_logo_url !!}' : '{!! $logo_url !!}'" alt="{{ __(':app_name Logo', ['app_name' => config('app.name')]) }}" {{ $attributes->merge(['class' => 'w-auto']) }} width="{{ $width }}" height="{{ $height }}" />
+    <x-img src="{!! $darkmode ? $darkmode_logo_url : $logo_url !!}" x-bind:src="darkMode ? '{!! $darkmode_logo_url !!}' : '{!! $logo_url !!}'" alt="{{ __(':app_name Logo', ['app_name' => config('app.name')]) }}" {{ $attributes->merge(array_filter([
+        'class' => 'w-auto',
+        'width' => $width, 
+        'height' => $height,
+    ])) }} 
+    />
 @else
     <h2 class="font-semibold text-lg">{{ config('app.name') }}</h2>
 @endif
