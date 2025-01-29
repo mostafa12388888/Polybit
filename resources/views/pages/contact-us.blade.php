424af3bbd1540b0e4ef087002d183a359f19fe86
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2025-01-29 08:11:00 +0200

    Add gtag code to contact us page

--- resources/views/pages/contact-us.blade.php
+++ resources/views/pages/contact-us.blade.php
@@ -140,6 +140,23 @@
 </div>
 
 <x-slot name="scripts">
+    <!-- Event snippet for call site conversion page
+    In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+    <script>
+        function gtag_report_conversion(url) {
+            var callback = function () {
+                if (typeof(url) != 'undefined') {
+                    window.location = url;
+                }
+            };
+            gtag('event', 'conversion', {
+                'send_to': 'AW-16825848814/ng0MCM6ktpYaEO6nmNc-',
+                'event_callback': callback
+            });
+            return false;
+        }
+    </script>
+    
     @minifyInclude('schema.contact-us-schema', [
         'title' => __('Contact Us'),
     ])
