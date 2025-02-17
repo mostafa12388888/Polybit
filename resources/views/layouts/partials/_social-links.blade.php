6dc319ddac5ea4b0027a205ad777794c6302375b
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2025-02-17 17:33:30 +0200

    add nofollow rel to external links

--- resources/views/layouts/partials/_social-links.blade.php
+++ resources/views/layouts/partials/_social-links.blade.php
@@ -1,5 +1,5 @@
 @foreach (setting('social_links') ?: [] as $social_link)
-    <x-link styling="light" href="{!! $social_link !!}" class="!p-0 flex items-center justify-center w-10 h-10">
+    <x-link styling="light" href="{!! $social_link !!}" target="_blank" rel="noopener nofollow" class="!p-0 flex items-center justify-center w-10 h-10">
         @if (strpos(parse_url($social_link)['host'], 'fb.com') !== false || strpos(parse_url($social_link)['host'], 'facebook.com') !== false)
             <x-icons.facebook class="!w-5 !h-5 text-dark-600/90 dark:text-dark-300" />
             <span class="sr-only">{{ __('Facebook Link') }}</span>
