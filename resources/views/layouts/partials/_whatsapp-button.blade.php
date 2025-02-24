7f68812feb9b24c2f65abdad07f7bea55f48e1bf
Mahmoud Abdelhamid <m7moudm7mmed@gmail.com>
2025-02-24 09:40:39 +0200

    Make the whatsapp link open in a new tab

--- resources/views/layouts/partials/_whatsapp-button.blade.php
+++ resources/views/layouts/partials/_whatsapp-button.blade.php
@@ -3,7 +3,7 @@
     $whatsapp_number = str_replace('+', '', $whatsapp_number);
 @endphp
 
-<x-link x-init="setTimeout(() => {$el.classList.remove('opacity-0');}, 1000);" 
+<x-link target="_blank" rel="noopener nofollow" x-init="setTimeout(() => {$el.classList.remove('opacity-0');}, 1000);" 
     href="https://wa.me/{{ $whatsapp_number }}" 
     class="opacity-0 transition-opacity duration-700 fixed bottom-[5.4rem] sm:bottom-4 rtl:left-4 ltr:right-4 !rounded-full w-14 h-14 sm:w-16 sm:h-16 bg-[#25D366] flex justify-center items-center border-2 border-dark-100 z-10">
     <span class="text-white absolute w-4 h-4 sm:w-5 sm:h-5 flex justify-center items-center bg-red-500 top-0 text-xs font-bold rounded-full right-0 shadow-xs">1</span>
